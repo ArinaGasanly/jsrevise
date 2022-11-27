eb28fdd64ce6342731913505ebf60151b0fcc299
ArinaFasanly <arinakudinova111@yandex.ru>
2022-11-27 23:47:04 +0300

    changes in script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+ let title = "повторение";
+ let screens = "adaptive";
+ let screenPrice = 50;
+ rollback = "no";
+ fullPrice = 240;
+ adaptive = "screen";
+
+
+
+ alert("Первое сообщение");
+ console.log("Готово!");
